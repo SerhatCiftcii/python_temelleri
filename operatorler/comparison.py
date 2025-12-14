4e0d0010db00c43a0d4ba13c00fac5966c309f1f
Serhat <s.ciftci561@gmail.com>
2025-12-14 21:30:35 +0300

    comparison

---
+++ operatorler/comparison.py
@@ -0,0 +1,22 @@
+# email, password => databese
+# email == 'info@serhat.com' - paswword ==12345
+
+a,b,c,d=5,5,20,8
+
+sonuc= (a==b)
+print(sonuc) #true
+
+sonuc1= (a != b)
+print(sonuc1)
+
+username="serhat.com"
+password="1234"
+
+print(username=="serhat.com")
+
+print(a>=b)
+print(d <= b)
+
+print(True==1)
+ 
+
---
+++ operatorler/comparison_demo.py
@@ -0,0 +1,41 @@
+#girilenn 2 sayıdan hangisi daha büyüktür
+""" 
+print("Lütfen bilgilerinizi doğru giriniz..")
+sayi1=int(input ("sayi1 giriniz "))
+sayi2=int(input("pozitif2.sayıyı giriniz"))
+if sayi1>sayi2:
+    print("sayi1 sayi2den daha büyüktür")
+elif(sayi2>sayi1):
+    print("sayi2 daah büyüktür")
+else:
+    print("sayılar eşittir.")
+ """
+""" =>allta uzun hali
+    if else olmadan 
+    input ile sayıyı alıp
+    sonuc(sayi1>sayi2) #true döner
+    print(f"{sayi1} {sayi2} den büyüktür {sonc}")
+    
+    """
+
+
+
+#girilen bir sayının tek mi çift mi olduğunu yazdırın.
+sayi=int(input("girilen sayının tekmi çift old.bulma"))
+sonuc= (sayi%2) ==0 
+print(f"{sayi}.çifttir: {sonuc}") #sayı çiffit:true yada false
+
+
+#3- girilen bir sayının negatif pozitif durumunu yazdınız.
+
+
+
+# Kullanıcıdan 2 vize (%60) ve final tüzde40 notunu aldırıp ortalama hesaplayınız.
+#eğer ortalama 50 ve üstüysse geçti değlse kaldı
+
+
+
+
+#parola ve email isteyip dorğuğnu kontrol ediniz.
+
+
