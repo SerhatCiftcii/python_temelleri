2273a15f4c361e1324890d194943b857f0213f42
Serhat <s.ciftci561@gmail.com>
2025-12-15 23:52:38 +0300

    for_demo_while add

--- Döngüler/for_demo.py
+++ Döngüler/for_demo.py
@@ -8,9 +8,33 @@ sayilar= [0,5,8,32,23,35,26,53,98]
         print("5in katı olan sayılar: ",sayi)"""
         
 #SAYİLAR listesindeki sayıların toplamı kaçtır?
-toplam=0
+"""toplam=0
 for sayiTopla in sayilar:
     toplam=toplam+sayiTopla
-print("sayiların toplamı: ",toplam)
+print("sayiların toplamı: ",toplam)"""
 
 #sayilar listesindeki çift sayıların karesini alınız.
+
+"""for sayi in sayilar:
+    if(sayi%2==0):
+        print("sayının karesi",sayi,sayi**2)"""
+        
+#urunler lisetsindeki tüm ürünlerin 2.karaterlerini ekrana yazdırma.
+# urunler=["elma","muz","kahve"]  
+# for urun in urunler:
+#     print("urunlerin2.karakteri: ",urun[1])  
+
+#urunler listesinde kahveden kaç adet geçmiştir. 
+
+"""urunler=["elma","muz","kahve","kahve"] 
+for kacTane in urunler:
+    if(kacTane in "kahve"):
+        print("vasa yazdır",kacTane) # 2 adaet varsa yazdır çıktır şimdi indexe göre yapalım"""
+
+urunler=["elma","muz","kahve","kahve","kahve"] 
+count=0
+for urun in urunler:
+    index=urun.find("kahve")
+    if(index>-1):
+        count +=1
+print(count)
---
+++ Döngüler/while.py
@@ -0,0 +1,18 @@
+sayilar=[2,5,7,9]
+#for collectıon liste üzerinedn işlem yapar lsite üzerinden elemanlar.
+#1-100 =>koşul=>while koşulla çalışır
+"""i=0
+while i<10: #sonsuz döngü break olmazsa küçüktür 10 bile olsa döenr sürekli false olması gerekir
+    print("merhaba",i)
+    i +=1 #bunu yazmamız gerek
+    """
+    
+    #strnglerde while
+    
+username=""
+while not username:
+    username =input("kullanıcı adınız")
+    
+print("kullanıcı adınız: ", username)
+    
+    
